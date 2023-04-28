84a9d80840486f7a57087ee3a8f6e3f6ff6a2c1e
showyha08 <over.and.uver@gmail.com>
2023-04-29 01:36:35 +0900

    add_devide

--- src/main/kotlin/TestClass.kt
+++ src/main/kotlin/TestClass.kt
@@ -2,4 +2,8 @@ class TestClass {
     fun multiply(x: Int, y: Int): Int {
         return x * y
     }
+
+    fun devide(x: Int, y: Int): Int {
+        return x / y
+    }
 }
--- src/test/kotlin/TestClassTest.kt
+++ src/test/kotlin/TestClassTest.kt
@@ -13,18 +13,36 @@ class TestClassTest {
     fun tearDown() {
     }
 
+
+    val tc = TestClass()
+
     @Test
     fun multiplyで1と2の乗算結果である2が取得できる() {
         val expected = 2
-        val actual = TestClass().multiply(1, 2)
+        val actual = tc.multiply(1, 2)
         assertThat(expected).isEqualTo(actual)
     }
 
     @Test
     fun multiplyで2と7の乗算結果である14が取得できる() {
         val expected = 14
-        val actual = TestClass().multiply(2, 7)
+        val actual = tc.multiply(2, 7)
         assertThat(expected).isEqualTo(actual)
     }
+
+    @Test
+    fun devideで4と2の除算結果が取得できる() {
+        val expected = 2
+        val actual = tc.devide(4, 2)
+        assertThat(expected).isEqualTo(actual)
+    }
+
+    @Test
+    fun devideで3と2の除算結果が取得できる() {
+        val expected = 1.5
+        val actual = tc.devide(3, 2)
+        assertThat(expected).isEqualTo(actual)
+    }
+
 
 }
